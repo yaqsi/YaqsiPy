b5f30e2d187192d2b03bd7461afc0693398d4231
yaqsi <58507225+yaqsi@users.noreply.github.com>
2021-03-24 16:33:40 +0400

    24-3

---
+++ UPyLib/UPyPyLib.py
@@ -0,0 +1,13 @@
+import math
+print("Program to demonstrate the use of Python Library")
+num = 121.121
+
+print("num=")
+print(num)
+print("=======================")
+print("The square root of num")
+print(math.sqrt(num))
+print("The ceilling of num")
+print(math.ceil(num))
+print("The floor of num")
+print(math.floor(num))
